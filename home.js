7fbf5a16ac05595cddfb1805d5aba4b56b0d3a74
jktfamily <76579653+jktfamily@users.noreply.github.com>
2021-02-25 13:58:18 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,23 @@
-fill(139,69,19);
+//sky
+background(0,255,255);
 
 var c = 200;
 
 var hc = 100;
 
+fill(255,255,0);
+
+//sun
+ellipse(0,0,250,250);
+
+fill(139,69,19);
+
+//house body
 rect(c-50,c,hc,hc);
 
-fill(0,255,0)
+fill(0,255,0);
 
+//ground
 rect(-100,300,4000,300);
+
+//v0.0.1
