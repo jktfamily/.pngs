fcd7ae84579e323475fa20917226ff0759b1bca8
jktfamily <76579653+jktfamily@users.noreply.github.com>
2021-02-26 08:24:49 +0800

    Update home.js

--- house/home.js
+++ house/home.js
@@ -7,20 +7,24 @@ var c = 200;
 
 var hc = 100;
 
-fill(255,255,0);
-
 //sun
+fill(255,255,0);
 ellipse(0,0,250,250);
 
-fill(139,69,19);
-
 //house
+fill(139,69,19);
 triangle(110,200,200,140,290,200);
 rect(c-50,c,hc,hc);
-
-fill(0,255,0);
+  //door
+  stroke(0,0,0);
+  arc(200,300,44,93,180,360);
+    //knob
+    fill(0,0,0);
+    ellipse(190,280,5,5);
 
 //ground
+noStroke();
+fill(0,255,0);
 rect(-100,300,4000,300);
 
-//v0.6
+//v0.7
